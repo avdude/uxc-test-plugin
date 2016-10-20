9ddfd07f01f3b3c992c5fcaf8a8c64913d92bdee
avdude <buckfleming@comcast.net>
2016-10-20 10:40:11 -0700

    Add files via upload

---
+++ uxc-test.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @author David Fleming
+ * @copyright 2016
+ */
+
+/*
+	Plugin Name: UXC Test Plugin
+	Description: This is for updating your Wordpress plugin.
+	Version: 1.0.0
+	Author: David Fleming
+	Author URI: http://www.avude.com
+*/
+if( ! class_exists( 'UXC_Test_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+$updater = new UXC_Test_Updater( __FILE__ );
+$updater->set_username( 'avdude' );
+$updater->set_repository( 'uxc-test-plugin ' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize();
+
+?>
